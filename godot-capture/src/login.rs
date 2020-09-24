aaab6109aa2977c5525c63b4637a08e0cb19a8bd
Eric Smith <paytonrules@gmail.com>
2020-09-24 09:43:19 -0500

    Login opens the GITLAB URI
    This starts the OAuth process

--- godot-capture/src/login.rs
+++ godot-capture/src/login.rs
@@ -1,6 +1,8 @@
 use gdnative::api::OS;
 use gdnative::prelude::*;
 
+const GITLAB_URI: &str = "https://gitlab.com/oauth/authorize?client_id=1ec97e4c1c7346edf5ddb514fdd6598e304957b40ca5368b1f191ffc906142ba&redirect_uri=http://127.0.0.1:8000/capture/&response_type=token&state=100&scope=api";
+
 #[derive(NativeClass)]
 #[inherit(Button)]
 pub struct Login;
@@ -14,15 +16,15 @@ impl Login {
     #[export]
     fn _button_pressed(&self, _owner: TRef<Button>) {
         /*        owner
-        .get_tree()
-        .map(|tree| unsafe { tree.assume_safe() })
-        .map(|tree| {
-            tree.change_scene("res://CaptureNote.tscn");
+            .get_tree()
+            .map(|tree| unsafe { tree.assume_safe() })
+            .map(|tree| {
+                tree.change_scene("res://CaptureNote.tscn");
         });*/
-        let url = "https://gitlab.example.com/oauth/authorize?client_id=APP_ID&redirect_uri=REDIRECT_URI&response_type=token&state=YOUR_UNIQUE_STATE_HASH&scope=REQUESTED_SCOPES";
-        OS::godot_singleton().shell_open(url).expect("should open");
-        //
-        //
+
+        OS::godot_singleton()
+            .shell_open(GITLAB_URI)
+            .expect("should open");
     }
 }
 
