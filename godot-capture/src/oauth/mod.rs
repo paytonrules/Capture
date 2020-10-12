da0d8c54053ecc88d83fa8142d8b1b9a3fb8e09c
Eric Smith <paytonrules@gmail.com>
2020-10-12 07:58:34 -0500

    Start the webserver on a random free port

--- godot-capture/src/oauth/mod.rs
+++ godot-capture/src/oauth/mod.rs
@@ -6,6 +6,7 @@ pub mod login_site;
 pub trait WebServer {
     fn token_sender(self, sender: SyncSender<String>) -> Self;
     fn launch(self);
+    fn port(&self) -> u16;
 }
 
 #[derive(Debug, PartialEq, Error)]
@@ -52,6 +53,9 @@ impl WebServer for RocketWebServer {
     fn launch(self) {
         self.rocket.launch();
     }
+    fn port(&self) -> u16 {
+        self.rocket.config().port
+    }
 }
 
 pub struct OAuthProvider;
@@ -61,17 +65,20 @@ impl OAuthProvider {
         OAuthProvider
     }
 
-    pub fn provide<T: WebServer + Send + Sync + 'static>(&self, server: T) -> Receiver<String> {
+    pub fn provide<T: WebServer + Send + Sync + 'static>(
+        &self,
+        server: T,
+    ) -> (Receiver<String>, String) {
         let (send, recv) = sync_channel(1);
 
         let server = server.token_sender(send);
-
-        // Give it a port
+        let login_url = format!("https://gitlab.com/oauth/authorize?client_id=1ec97e4c1c7346edf5ddb514fdd6598e304957b40ca5368b1f191ffc906142ba&redirect_uri=http://127.0.0.1:{}/capture/&response_type=token&state=100&scope=api",
+                       server.port());
 
         thread::spawn(move || {
             server.launch();
         });
-        recv
+        (recv, login_url)
     }
 }
 
@@ -82,11 +89,15 @@ mod tests {
     #[derive(Clone)]
     struct MocketWrapper {
         sync_sender: Option<SyncSender<String>>,
+        port: u16,
     }
 
     impl MocketWrapper {
         fn new() -> Self {
-            MocketWrapper { sync_sender: None }
+            MocketWrapper {
+                sync_sender: None,
+                port: 0,
+            }
         }
     }
 
@@ -100,23 +111,33 @@ mod tests {
             self.sync_sender
                 .map(|sender| sender.send("token".to_string()));
         }
-    }
 
-    impl RocketWebServer {
-        pub fn port(&self) -> u16 {
-            self.rocket.config().port
+        fn port(&self) -> u16 {
+            self.port
         }
     }
 
     #[test]
-    fn launches_provider_on_start() {
+    fn launches_webserver_on_provide() {
         let server = OAuthProvider::new();
         let mock_server = MocketWrapper::new();
 
-        let receiver = server.provide(mock_server.clone());
+        let (receiver, _url) = server.provide(mock_server.clone());
         let token = receiver.recv_timeout(std::time::Duration::from_millis(10));
 
         assert_eq!("token".to_string(), token.unwrap());
+    }
+
+    #[test]
+    fn returns_url_for_login_on_provide_with_port() {
+        let server = OAuthProvider::new();
+        let mut mock_server = MocketWrapper::new();
+        mock_server.port = 10000;
+
+        let (_receiver, url) = server.provide(mock_server.clone());
+
+        assert!(url.starts_with("https://gitlab.com/oauth/authorize"));
+        assert!(url.contains("&redirect_uri=http://127.0.0.1:10000/capture/"))
     }
 
     #[test]
