6a260ff5c237d42a0f68df0c35359d76cef1dbb8
Eric Smith <paytonrules@gmail.com>
2020-10-12 07:58:54 -0500

    Advance to the next scene when we get a token
    This uses signals to emit the constructed redirect URL (with a free random
    port) and the received token when the user logs in.
    
    When we get the token, we advance to the next scene!

--- godot-capture/src/nodes/login.rs
+++ godot-capture/src/nodes/login.rs
@@ -1,30 +1,42 @@
 use gdnative::api::OS;
 use gdnative::prelude::*;
 
-const GITLAB_URI: &str = "https://gitlab.com/oauth/authorize?client_id=1ec97e4c1c7346edf5ddb514fdd6598e304957b40ca5368b1f191ffc906142ba&redirect_uri=http://127.0.0.1:8080/capture/&response_type=token&state=100&scope=api";
-
 #[derive(NativeClass)]
 #[inherit(Button)]
-pub struct Login;
+pub struct Login {
+    login_url: Option<String>,
+}
 
 #[methods]
 impl Login {
     fn new(_owner: &Button) -> Self {
-        Login
+        Login { login_url: None }
+    }
+
+    #[export]
+    fn _login_url_received(&mut self, _owner: TRef<Button>, url: Variant) {
+        self.login_url = url.try_to_string();
     }
 
     #[export]
     fn _button_pressed(&self, _owner: TRef<Button>) {
-        /*        owner
+        if let Some(login_url) = &self.login_url {
+            OS::godot_singleton()
+                .shell_open(login_url)
+                .expect("should open");
+        }
+    }
+
+    //
+    #[export]
+    fn _token_received(&self, owner: TRef<Button>, _token: Variant) {
+        owner
             .get_tree()
             .map(|tree| unsafe { tree.assume_safe() })
             .map(|tree| {
-                tree.change_scene("res://CaptureNote.tscn");
-        });*/
-
-        OS::godot_singleton()
-            .shell_open(GITLAB_URI)
-            .expect("should open");
+                tree.change_scene("res://CaptureNote.tscn")
+                    .expect("Should change scene");
+            });
     }
 }
 
--- godot-capture/src/nodes/oauth.rs
+++ godot-capture/src/nodes/oauth.rs
@@ -5,6 +5,7 @@ use std::sync::mpsc::Receiver;
 
 #[derive(NativeClass)]
 #[inherit(Node)]
+#[register_with(Self::register_signals)]
 pub struct OAuthValidation {
     token_receiver: Option<Receiver<String>>,
 }
@@ -17,21 +18,47 @@ impl OAuthValidation {
         }
     }
 
+    fn register_signals(builder: &ClassBuilder<Self>) {
+        builder.add_signal(Signal {
+            name: "login_url_ready",
+            args: &[SignalArgument {
+                name: "url",
+                default: Variant::from_str(""),
+                export_info: ExportInfo::new(VariantType::GodotString),
+                usage: PropertyUsage::DEFAULT,
+            }],
+        });
+
+        builder.add_signal(Signal {
+            name: "token_received",
+            args: &[SignalArgument {
+                name: "token",
+                default: Variant::from_str(""),
+                export_info: ExportInfo::new(VariantType::GodotString),
+                usage: PropertyUsage::DEFAULT,
+            }],
+        });
+    }
+
     #[export]
-    fn _ready(&mut self, _owner: TRef<Node>) {
+    fn _ready(&mut self, owner: TRef<Node>) {
         let provider = OAuthProvider::new();
         let port = port_check::free_local_port();
         match RocketWebServer::builder().port(port).build() {
-            Ok(rocket) => self.token_receiver = Some(provider.provide(rocket)),
-            Err(err) => godot_print!("Error {:?} building rocket", err),
+            Ok(rocket) => {
+                let (token_receiver, url) = provider.provide(rocket);
+                self.token_receiver = Some(token_receiver);
+                owner.emit_signal("login_url_ready", &[Variant::from_str(url)]);
+            }
+            Err(err) => godot_error!("Error {:?} building rocket", err),
         };
     }
 
     #[export]
-    fn _process(&self, _owner: TRef<Node>, _delta: f64) {
+    fn _process(&self, owner: TRef<Node>, _delta: f64) {
         if let Some(token_receiver) = &self.token_receiver {
-            if let Ok(_token) = token_receiver.try_recv() {
-                godot_print!("token came back! (but lets not print it)");
+            if let Ok(token) = token_receiver.try_recv() {
+                owner.emit_signal("token_received", &[Variant::from_str(token)]);
             }
         }
     }
