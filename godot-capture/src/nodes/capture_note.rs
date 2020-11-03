724048dba0740eb8962f820aa69da6f0d9bc644e
Eric Smith <paytonrules@gmail.com>
2020-11-03 08:32:42 -0600

    Only show four latest TODOs
    Use get_node with "/root" so you don't need explicit calls.

--- godot-capture/src/nodes/capture_note.rs
+++ godot-capture/src/nodes/capture_note.rs
@@ -39,21 +39,15 @@ impl Remember {
     }
 
     #[export]
-    fn _ready(&mut self, owner: &TextureButton) {
+    fn _ready(&mut self, owner: TRef<TextureButton>) {
         match self.load_todos() {
             Ok(todos) => {
                 let label = owner
-                    .get_tree()
-                    .map(|tree| unsafe { tree.assume_safe() })
-                    .and_then(|tree| tree.root())
-                    .map(|root| unsafe { root.assume_safe() })
-                    .and_then(|root| {
-                        root.get_node("./CaptureNote/CenterContainer/VBoxContainer/Recent Todos")
-                    })
+                    .get_node("/root/CaptureNote/CenterContainer/VBoxContainer/Recent Todos")
                     .map(|node| unsafe { node.assume_safe() })
                     .and_then(|node| node.cast::<Label>())
-                    .unwrap();
-                label.set_text(todos.inbox.clone());
+                    .expect("Recent Todos node is missing");
+                label.set_text(get_latest_todos(&todos.inbox));
                 self.todo = Some(todos);
             }
             Err(err) => godot_error!("Error! {:?}", err),
@@ -73,5 +67,53 @@ impl Remember {
     #[export]
     fn _save_me(&mut self, _owner: TRef<TextureButton>) {
         godot_print!("Save me, SAVE me, SAAAAAAAAVE MEEEEEE");
+    }
+}
+
+fn get_latest_todos(inbox: &String) -> String {
+    let todos = inbox
+        .split('\n')
+        .map(|str| str.to_string())
+        .collect::<Vec<String>>();
+    let todo_count = todos.len();
+
+    todos
+        .iter()
+        .skip(todo_count - 4)
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+        .join("\n")
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn take_all_items_up_to_four() {
+        let full_list = "- one
+- two
+- three
+- four"
+            .to_string();
+        assert_eq!(full_list, get_latest_todos(&full_list));
+    }
+
+    #[test]
+    fn take_only_the_last_four() {
+        let full_list = "- skip
+- one
+- two
+- three
+- four"
+            .to_string();
+        let expected = "- one
+- two
+- three
+- four"
+            .to_string();
+
+        assert_eq!(expected, get_latest_todos(&full_list));
     }
 }
