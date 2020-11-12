951604d8027e53490210f49db80a30724c243f5b
Eric Smith <paytonrules@gmail.com>
2020-11-12 08:01:31 -0600

    Rename todo.rs to inbox.rs
    The great renaming continues.

--- godot-capture/src/inbox/mod.rs
+++ godot-capture/src/inbox/mod.rs
@@ -1,8 +1,8 @@
 mod decoder;
+mod inbox;
 mod mock_storage;
 mod storage;
-mod todo;
+pub use inbox::Todo;
+pub use inbox::TodoError;
 pub use mock_storage::{MockError, MockStorage};
 pub use storage::{GitlabStorage, Storage};
-pub use todo::Todo;
-pub use todo::TodoError;
