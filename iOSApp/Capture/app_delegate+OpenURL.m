5485215de8dabf747e0d09482281f432981c55d1
Eric Smith <paytonrules@gmail.com>
2020-11-24 07:31:34 -0600

    Add AppDelegate category to detect login
    On iOS when the user logs in the browser redirects to
    paytonrules.Capture:// - which is not a valid URL but does open this app
    because it is registered with a custom URL scheme.
    
    The app opens, and we detect it in our new AppDelegate which is the same
    one that Godot created but with a new category (OpenURL) to add the
    callback.
    
    This required adding header files from Godot to the project, so they
    could be referenced.

---
+++ iOSApp/Capture/app_delegate+OpenURL.m
@@ -0,0 +1,24 @@
+//
+//  NSObject+UIViewController_TestSwizzle.m
+//  Capture
+//
+//  Created by Eric Smith on 11/19/20.
+//  Copyright © 2020 GodotEngine. All rights reserved.
+//
+#import <objc/runtime.h>
+#import <UIKit/UIKit.h>
+#import "app_delegate.h"
+
+@implementation AppDelegate (OpenURL)
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    NSMutableDictionary *dic = [NSMutableDictionary new];
+    for (NSString *segment in [url.fragment componentsSeparatedByString:@"&"]) {
+        // key=value
+        NSArray<NSString*> *pair = [segment componentsSeparatedByString:@"="];
+        dic[pair[0]] = pair[1];
+    }
+    
+    return TRUE;
+}
+@end
