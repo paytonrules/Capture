e4f1ef2fe3ccd42d5f98acb7eb87f206e18a2c20
Eric Smith <paytonrules@gmail.com>
2020-12-01 07:59:05 -0600

    On OpenURL call into our rust library
    When the iOS app's OAuth flow calls back into Capture, with a custom
    URL, we call into the rust library with its fragment.
    
    This currently does nothing, as we need to refactor the code so we can
    send the token to the channel that is waiting for it.

--- godot-capture/src/lib.rs
+++ godot-capture/src/lib.rs
@@ -8,6 +8,8 @@ use gdnative::prelude::*;
 use nodes::capture_note::Remember;
 use nodes::login::Login;
 use nodes::oauth::OAuthValidation;
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
 #[macro_use]
 extern crate rocket;
@@ -19,3 +21,9 @@ fn init(handle: InitHandle) {
 }
 
 godot_init!(init);
+
+#[no_mangle]
+pub extern "C" fn logged_in(fragment: *const c_char) {
+    let fragment = unsafe { CStr::from_ptr(fragment) };
+    println!("Logged In fragment: {:#?}", fragment.to_str());
+}
--- iOSApp/Capture/app_delegate+OpenURL.mm
+++ iOSApp/Capture/app_delegate+OpenURL.mm
@@ -1,26 +1,22 @@
 //
-//  NSObject+UIViewController_TestSwizzle.m
+//  AppDelegate+OpenUrl.mm
 //  Capture
 //
 //  Created by Eric Smith on 11/19/20.
-//  Copyright © 2020 GodotEngine. All rights reserved.
+//  Copyright © 2020 Eric Smith. All rights reserved.
 //
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 #import "app_delegate.h"
-#import "core/engine.h"
+#import <iostream>
+#import "godot_capture.h"
 
 @implementation AppDelegate (OpenURL)
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    NSMutableDictionary *dic = [NSMutableDictionary new];
-    for (NSString *segment in [url.fragment componentsSeparatedByString:@"&"]) {
-        // key=value
-        NSArray<NSString*> *pair = [segment componentsSeparatedByString:@"="];
-        dic[pair[0]] = pair[1];
-    }
-    NSLog(@"openURL happened. All is well!");
-    
+
+    logged_in([url.fragment cStringUsingEncoding:NSUTF8StringEncoding]);
+
     return TRUE;
 }
 @end
