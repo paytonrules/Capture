aa43a949301d5f22ce5537ccbad4b6777fac472d
Eric Smith <paytonrules@gmail.com>
2020-12-15 07:09:15 -0600

    Safely parse the access token
    This added a bunch of tests and error handling.

--- godot-capture/src/lib.rs
+++ godot-capture/src/lib.rs
@@ -23,20 +23,31 @@ godot_init!(init);
 
 #[no_mangle]
 pub extern "C" fn logged_in(fragment: *const c_char) {
-    let fragment = unsafe { CStr::from_ptr(fragment) }.to_str();
-    let fragment = fragment.unwrap();
-    let token = fragment
-        .split('&')
-        .find(|segment| segment.starts_with("access_token"));
-    let token = token.unwrap().split('=').last().unwrap().to_string();
+    if !fragment.is_null() {
+        if let Ok(fragment) = unsafe { CStr::from_ptr(fragment) }.to_str() {
+            save_access_token(fragment);
+        }
+    }
+}
 
-    save_token(token);
+fn save_access_token(fragment: &str) -> anyhow::Result<()> {
+    match fragment
+        .split('&')
+        .find(|segment| segment.starts_with("access_token"))
+        .and_then(|pair| pair.split('=').nth(1))
+    {
+        Some(token) => {
+            save_token(token.to_string());
+            Ok(())
+        }
+        None => anyhow::bail!("Error parsing access token"),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use oauth::{clear_token, get_token};
+    use oauth::{clear_token, get_token, TokenError};
     use serial_test::serial;
     use std::ffi::CString;
 
@@ -50,6 +61,39 @@ mod tests {
         logged_in(fragment?.as_ptr());
 
         assert_eq!("passed_in_token", get_token()?);
+        Ok(())
+    }
+
+    #[test]
+    #[serial(access_token)]
+    fn test_null_ptr_handling() {
+        clear_token();
+
+        logged_in(std::ptr::null());
+
+        assert_eq!(TokenError::NoTokenPresent, get_token().unwrap_err())
+    }
+
+    #[test]
+    #[serial(access_token)]
+    fn test_token_is_unchanged_when_access_token_is_not_present(
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        clear_token();
+
+        logged_in(CString::new("no_token")?.as_ptr());
+
+        assert_eq!(TokenError::NoTokenPresent, get_token().unwrap_err());
+        Ok(())
+    }
+
+    #[test]
+    #[serial(access_token)]
+    fn test_token_is_unchanged_when_token_is_malformed() -> Result<(), Box<dyn std::error::Error>> {
+        clear_token();
+
+        logged_in(CString::new("access_token&state=100")?.as_ptr());
+
+        assert_eq!(TokenError::NoTokenPresent, get_token().unwrap_err());
         Ok(())
     }
 }
